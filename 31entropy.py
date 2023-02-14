d39fdd3fb08d7ff7f30a60e5c5ae5cbb8cc0a532
Nicole Waring <nicole@Nicoles-MacBook-Pro-83.local>
2023-02-13 23:31:54 -0800

    submitting homework

--- 31entropy.py
+++ 31entropy.py
@@ -15,3 +15,26 @@
 python3 31entropy.py 0.1 0.2 0.3 0.4
 1.846
 """
+
+import math 
+import sys
+
+
+list =[]
+for value in sys.argv[1:]:
+	try:
+		list.append(float(value))
+	except:
+		raise ValueError(f'One or more inputted values not a number.')
+
+try:
+	assert(math.isclose(sum(list), 1.0))
+except:
+		raise ValueError(f'Sum of inputted values does not add to 1.0.')
+		
+SE_sum = 0
+for value in list:
+	SE_sum += (value) * ((math.log((1/value), 2)))
+	
+
+print (round(SE_sum, 3))
