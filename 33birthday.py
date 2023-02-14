2b5276a5ab755d02e06dc22e0def32b4880d9c8e
Nicole Waring <nicole@Nicoles-MacBook-Pro-83.local>
2023-02-13 23:33:24 -0800

    submitting homework

--- 33birthday.py
+++ 33birthday.py
@@ -16,3 +16,64 @@ python3 33birthday.py 365 23
 0.571
 """
 
+import sys
+import random
+
+days = int(sys.argv[1])
+people = int(sys.argv[2])
+
+trials = 10000
+
+#calendar a list
+TrueSum = 0
+calendar = [0] * days
+
+for i in range(trials):
+	for i in range(people):
+		bday = random.randint(0,days - 1)
+		calendar[bday] += 1
+    
+
+	for i in range(len(calendar)):
+		if calendar[i] > 1:
+			success = True
+			break  
+		else:
+			success = False
+
+	if success == True:
+		TrueSum += 1 
+
+	
+	calendar = [0] * days
+    	
+print(TrueSum/trials)
+
+
+#birthdays a list 
+
+TrueSum = 0
+birthdays = []
+samebd = []
+for i in range(trials):
+	for i in range(people):
+		bday = random.randint(0, days - 1)
+		birthdays.append(bday)
+		
+	for j in birthdays:
+		if j not in samebd:
+			samebd.append(j)
+		else:
+			success = True
+			TrueSum += 1 
+			break 
+	
+	
+	birthdays = []
+	samebd = []
+
+print(TrueSum/trials)
+		
+
+		
+
