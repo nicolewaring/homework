7aa78d96ae8838d3628effb89abc71e4c4bd0fff
Nicole Waring <nicole@Nicoles-MacBook-Pro-83.local>
2023-03-11 10:28:44 -0800

    submitting homework

--- 40aacomp.py
+++ 40aacomp.py
@@ -33,3 +33,128 @@ V 94237 0.0709
 W 20297 0.0153
 Y 37628 0.0283
 """
+
+import gzip 
+import sys 
+
+sumA = 0
+sumC = 0
+sumD = 0
+sumE = 0
+sumF = 0
+sumG = 0
+sumH = 0
+sumI = 0
+sumK = 0
+sumL = 0
+sumM = 0
+sumN = 0
+sumP = 0
+sumQ = 0
+sumR = 0
+sumS = 0
+sumT = 0
+sumV = 0
+sumW = 0
+sumY =0
+total = 0 
+
+with gzip.open(sys.argv[1], 'rt') as fp: 
+	for line in fp.readlines():
+		if line.startswith(">"):
+			continue
+		for char in line:
+			total += 1
+			if char == 'A':
+				sumA += 1
+			elif char == 'C':
+				sumC += 1
+			elif char == 'D':
+				sumD += 1
+			elif char == 'E':
+				sumE += 1
+			elif char == 'F':
+				sumF += 1
+			elif char == 'G':
+				sumG += 1
+			elif char == 'H':
+				sumH += 1
+			elif char == 'I':
+				sumI += 1 
+			elif char == 'K':
+				sumK += 1
+			elif char == 'L':
+				sumL += 1
+			elif char == 'M':
+				sumM += 1
+			elif char == 'N':
+				sumN += 1
+			elif char == 'P':
+				sumP += 1
+			elif char == 'Q':
+				sumQ += 1
+			elif char == 'R':
+				sumR += 1
+			elif char == 'S':
+				sumS += 1 
+			elif char == 'T':
+				sumT += 1
+			elif char == 'V':
+				sumV += 1 
+			elif char == 'W':
+				sumW += 1
+			elif char == 'Y':
+				sumY += 1
+print('A', sumA, round(sumA/total, 4))
+print('C', sumC, round(sumC/total, 4))
+print('D', sumD, round(sumD/total, 4)
+print('E', sumE, round(sumE/total, 4))
+print('F', sumF, round(sumF/total, 4))
+print('G', sumG, round(sumG/total, 4))
+print('H', sumH, round(sumH/total, 4))
+print('I', sumI, round(sumI/total, 4))
+print('K', sumK, round(sumK/total, 4))
+print('L', sumL, round(sumL/total, 4))
+print('M', sumM, round(sumM/total, 4))
+print('N', sumN, round(sumN/total, 4))
+print('P', sumP, round(sumA/total, 4))
+print('Q', sumQ, round(sumQ/total, 4))
+print('R', sumR, round(sumR/total, 4))
+print('S', sumS, round(sumS/total, 4))
+print('T', sumT, round(sumT/total, 4))
+print('V', sumV, round(sumV/total, 4))
+print('W', sumW, round(sumW/total, 4))
+print('Y', sumY, round(sumY/total, 4))
+
+
+
+
+#list variation
+
+s = 'ACDEFGHIKLMNPQRSTVWY'
+l = 20 * [0]
+total = 0 
+with gzip.open(sys.argv[1], 'rt') as fp:
+	for line in fp.readlines():
+		if line.startswith(">"):
+			continue
+		print(line)
+		for char in line:
+			total += 1 
+			for i in range(len(s)):
+				if s[i] == char:
+					l[i] += 1
+					continue 
+for j in range(len(l)):
+	print(s[j], l[j], round(l[j]/total, 4))
+		
+		
+		
+		
+		
+#string method - find/index methods 
+
+					
+
+
+						
