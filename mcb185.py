667501b820cac73e629426bded9aeb0a6fbb06ec
Nicole Waring <nicole@Nicoles-MacBook-Pro-83.local>
2023-03-17 18:23:41 -0700

    submitting for 60translate.py

--- mcb185.py
+++ mcb185.py
@@ -15,14 +15,14 @@ def read_fasta(filename):
 	while True:
 		line = fp.readline();
 		if line == '': break
-		line = line.rstrip()
+		line = line.rstrip()  
 		if line.startswith('>'):
-			if len(seqs) > 0:
-				yield(name, ''.join(seqs))
+			if len(seqs) > 0: #checking if there's any sequence
+				yield(name, ''.join(seqs)) #previous sequence is done
 				name = line[1:]
 				seqs = []
 			else:
-				name = line[1:]
+				name = line[1:] #line after the >
 		else:
 			seqs.append(line)
 
@@ -30,3 +30,31 @@ def read_fasta(filename):
 	fp.close()
 
 # def other functions...
+
+gcode = {
+	'AAA' : 'K',	'AAC' : 'N',	'AAG' : 'K',	'AAT' : 'N',
+	'ACA' : 'T',	'ACC' : 'T',	'ACG' : 'T',	'ACT' : 'T',
+	'AGA' : 'R',	'AGC' : 'S',	'AGG' : 'R',	'AGT' : 'S',
+	'ATA' : 'I',	'ATC' : 'I',	'ATG' : 'M',	'ATT' : 'I',
+	'CAA' : 'Q',	'CAC' : 'H',	'CAG' : 'Q',	'CAT' : 'H',
+	'CCA' : 'P',	'CCC' : 'P',	'CCG' : 'P',	'CCT' : 'P',
+	'CGA' : 'R',	'CGC' : 'R',	'CGG' : 'R',	'CGT' : 'R',
+	'CTA' : 'L',	'CTC' : 'L',	'CTG' : 'L',	'CTT' : 'L',
+	'GAA' : 'E',	'GAC' : 'D',	'GAG' : 'E',	'GAT' : 'D',
+	'GCA' : 'A',	'GCC' : 'A',	'GCG' : 'A',	'GCT' : 'A',
+	'GGA' : 'G',	'GGC' : 'G',	'GGG' : 'G',	'GGT' : 'G',
+	'GTA' : 'V',	'GTC' : 'V',	'GTG' : 'V',	'GTT' : 'V',
+	'TAA' : '*',	'TAC' : 'Y',	'TAG' : '*',	'TAT' : 'Y',
+	'TCA' : 'S',	'TCC' : 'S',	'TCG' : 'S',	'TCT' : 'S',
+	'TGA' : '*',	'TGC' : 'C',	'TGG' : 'W',	'TGT' : 'C',
+	'TTA' : 'L',	'TTC' : 'F',	'TTG' : 'L',	'TTT' : 'F',
+}
+
+def translate(cds):
+	cds=cds.upper()
+	for nt in range(0,len(cds)-3+1,3):
+		win=cds[nt:nt+3]
+		for key in gcode:
+			if key==win:
+				print(gcode[key], end="")
+	
