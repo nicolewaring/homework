db529a209fb3855d8d3656f282cb998f5abd12ab
Nicole Waring <nicole@Nicoles-MBP-83.attlocal.net>
2023-03-24 22:20:27 -0700

    submitting homework

--- 62orfs.py
+++ 62orfs.py
@@ -17,7 +17,7 @@
 
 
 """
-python3 62orfs.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.fna.gz
+
 NC_000913.3 108 500 - MVFSIIATRW
 NC_000913.3 337 2799 + MRVLKFGGTS
 NC_000913.3 2801 3733 + MVKVYAPASS
@@ -29,3 +29,55 @@ NC_000913.3 5683 6459 - MLILISPAKT
 NC_000913.3 6529 7959 - MPDFFSFINS
 NC_000913.3 7366 7773 + MKTASDCQQS
 """
+
+import argparse 
+import mcb185 
+
+parser=argparse.ArgumentParser(description="Finds ORF's in E.coli Genome")
+
+#positional argument 
+parser.add_argument('file', type=str, metavar='file', help='fasta file')
+
+#optional arguments with default parameters
+parser.add_argument('-m', required=False, type=int, default= 300, metavar='ORF size', 
+	help='ORF size')
+
+arg=parser.parse_args()
+
+for defline, seq in mcb185.read_fasta(arg.file):
+	neg=mcb185.revcomp(seq)
+	
+	seqs=[seq]
+
+	
+	for strand in seqs:
+		for frame in range(3):
+			in_ORF=False
+			for j in range(frame, len(seq), 3):
+				codon=mcb185.translate(seq[j:j+3])
+				if codon=='M' and not in_ORF:
+					in_ORF=True
+					startcoord=j
+					
+				if in_ORF and codon=='*':
+					cds=mcb185.translate(strand[startcoord:j+3])
+					if len(cds)>arg.m:
+						print(defline, startcoord+1, j+3, '+', cds[:10])		
+
+	negs=[neg]
+	for strand in negs:
+		for frame in range(3):
+			in_ORF=False
+			for j in range(frame, len(seq), 3):
+				codon=mcb185.translate(seq[j:j+3])
+				if codon=='M' and not in_ORF:
+					in_ORF=True
+					startcoord=j
+					
+				if in_ORF and codon=='*':
+					cds=mcb185.translate(strand[startcoord:j+3])
+					if len(cds)>arg.m:
+						print(defline, len(seq)-startcoord +1, len(seq)-j+3, '-', cds[:10])
+	
+		
+	
